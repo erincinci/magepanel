d7496b033db8c6568d60cc43abce89f8a35cec5f
Erinç İnci <erincinci@gmail.com>
2014-07-23 17:22:57 +0300

    ansi-to-html newline tryout

--- routes/mage.js
+++ routes/mage.js
@@ -63,6 +63,7 @@ exports.command = function(req, res) {
         }
         if (stdout) {
             console.debug(stdout);
+            convert.newline = true;
             output = convert.toHtml(stdout);
         }
 
