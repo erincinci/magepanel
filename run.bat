f57fe76dbc408f4907810902c0ee1ce3b717b456
Erinç İnci <erincinci@gmail.com>
2014-08-18 10:45:06 +0300

    packages.json updated, npm winservice scripts added

--- run.bat
+++ run.bat
@@ -18,6 +18,9 @@ IF %ERRORLEVEL% NEQ 0 ECHO node is not installed in system, please install it be
 WHERE nodemon
 IF %ERRORLEVEL% NEQ 0 ECHO nodemon is not installed in system, installing & call npm install nodemon -g & echo.
 
+WHERE winser
+IF %ERRORLEVEL% NEQ 0 ECHO winser is not installed in system, installing & call npm install winser -g & echo.
+
 ::::::::::::::::::::::::: Update Module Dependencies :::::::::::::::::::::::::
 echo Update module dependencies if necessary..
 echo.
